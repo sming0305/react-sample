eedf5a71be657a9d1c313c5622076561113dca0b
sming0305 <sming0305@gmail.com>
2023-08-04 18:45:32 +0800

    fix : vite.config 調整

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 此處用來加上預設的路徑，也就是"基本路徑"
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === "production" ? "/react-sample/" : "/",
   plugins: [react()],
-})
+});
